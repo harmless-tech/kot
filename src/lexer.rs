466b041c47cd1e3e1e1229cf4bbabd182d6513d4
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-12-12 15:27:26 -0500

    Added parsing for numbers, char, and some strings

--- src/lexer.rs
+++ src/lexer.rs
@@ -16,7 +16,7 @@ impl PosToken {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum Token {
     /// Starts with [_ A..Z a..z], then [_ - A..Z a..z 0..9]
     Ident(String),
@@ -32,15 +32,22 @@ pub enum Token {
     NumberBinary(String),
     /// '\n', 'c'
     Charater(char),
-    /// "", "string"
+    /// "", "string" <br>
+    /// \`back string\`, <br>
+    /// \`back \ <br>
+    /// string\` == "back string" <br>
+    /// r"", r#" " "#
     String(String),
+    // TODO: Remove old string types
     /// \`back string\`, <br>
     /// \`back \ <br>
     /// string\` == "back string"
+    #[deprecated(note="Use `String` instead")]
     BackString(String),
     /// r"", r#" " "#
+    #[deprecated(note="Use `String` instead")]
     RawString(String),
-    Eof,
+    EOF,
 
     /// (
     LParan,
@@ -195,7 +202,7 @@ pub enum Token {
     RangeInclusive,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum MacroToken {
     /// #config IDENT VALUE
     Config,
@@ -228,6 +235,8 @@ pub enum MacroToken {
     Regex,
 }
 
+type Tokens = Vec<PosToken>;
+
 struct Lexer {
     chars: Vec<char>,
     index: usize,
@@ -248,18 +257,12 @@ impl Lexer {
         self.index < self.chars.len()
     }
 
-//    fn get(&mut self) -> char {
-//        let c = self.peek_i( 0);
-//        self.index += 1;
-//        c
-//    }
-
     fn range(&self, range: Range<usize>) -> String {
         self.chars[range].iter().collect()
     }
 
     fn peek(&self) -> char {
-        self.peek_i( 0)
+        self.peek_i(0)
     }
 
     fn peek_i(&self, offset: usize) -> char {
@@ -311,10 +314,11 @@ pub fn lex(contents: &str) -> anyhow::Result<Vec<PosToken>> {
     }
 
     while lexer.within() {
-        match (lexer.peek(), lexer.peek_i(1)) {
+        let (c1, c2) = (lexer.peek(), lexer.peek_i(1));
+        match (c1, c2) {
             // Comments
-            ('/', '/') => remove_single_line_comment(lexer), // Single line
-            ('/', '*') => remove_multi_line_comment(lexer), // Multi line
+            ('/', '/') => skip_single_line_comment(lexer), // Single line
+            ('/', '*') => skip_multi_line_comment(lexer),  // Multi line
             // Singles
             ('(', _) => token1!(LParan),
             (')', _) => token1!(RParan),
@@ -383,20 +387,26 @@ pub fn lex(contents: &str) -> anyhow::Result<Vec<PosToken>> {
             ('.', '.') => match lexer.peek_i(2) {
                 '<' => token3!(RangeExclusive),
                 '=' => token3!(RangeInclusive),
-                _ => panic!("Lexer: Token '<' or '=' required after \"..\" at {}:{}.", lexer.line, lexer.col), // TODO: Return error instead of panic
+                _ => panic!(
+                    "Lexer: Token '<' or '=' required after \"..\" at {}:{}.",
+                    lexer.line, lexer.col
+                ), // TODO: Return error instead of panic
             },
             // Numbers
-            ('0', 'x') => todo!(),
-            ('0', 'b') => todo!(),
-            ('-' | '0'..='9', _) => todo!(),
+            ('0', 'x') => get_hex(lexer, &mut tokens),
+            ('0', 'b') => get_binary(lexer, &mut tokens),
+            ('-' | '0'..='9', _) => get_number(lexer, &mut tokens),
             // Char and Strings
-            ('\'', _) => todo!(),
-            ('"', _) => todo!(),
-            ('`', _) => todo!(),
+            ('\'', _) => get_char(lexer, &mut tokens),
+            ('"', _) => get_string(lexer, &mut tokens),
+            ('`', _) => get_back_string(lexer, &mut tokens),
             ('r', '#' | '"') => todo!(),
             // Macros
             ('#', 'a'..='z') => todo!(),
-            ('#', next) => panic!("Lexer: Unexpected token '{next}' after macro tag at {}:{}.", lexer.line, lexer.col), // TODO: Return error instead of panic
+            ('#', next) => panic!(
+                "Lexer: Unexpected token '{next}' after macro tag at {}:{}.",
+                lexer.line, lexer.col
+            ), // TODO: Return error instead of panic
             // Ident
             ('.', next)
                 if ('a'..='z').contains(&next) || ('A'..='Z').contains(&next) || next == '_' =>
@@ -412,11 +422,11 @@ pub fn lex(contents: &str) -> anyhow::Result<Vec<PosToken>> {
         }
     }
 
-    token!(Eof);
+    token!(EOF);
     Ok(tokens)
 }
 
-fn remove_single_line_comment(lexer: &mut Lexer) {
+fn skip_single_line_comment(lexer: &mut Lexer) {
     lexer.i(2);
     while lexer.within() {
         match lexer.peek() {
@@ -430,7 +440,7 @@ fn remove_single_line_comment(lexer: &mut Lexer) {
 }
 
 // TODO: No panic!
-fn remove_multi_line_comment(lexer: &mut Lexer) {
+fn skip_multi_line_comment(lexer: &mut Lexer) {
     let (line, col) = (lexer.line, lexer.col);
 
     lexer.i(2);
@@ -448,14 +458,274 @@ fn remove_multi_line_comment(lexer: &mut Lexer) {
     panic!("Lexer: Multi-line comment, starting at {line}:{col}, does not have an ending \"*/\".");
 }
 
+// TODO: No panic
+fn get_hex(lexer: &mut Lexer, tokens: &mut Tokens) {
+    let (line, col) = (lexer.line, lexer.col);
+
+    let mut builder = String::new();
+
+    lexer.i(2);
+    while lexer.within() {
+        let c = lexer.peek();
+        match c {
+            '0'..='9' | 'a'..='f' => {
+                builder.push(c);
+                lexer.i(1);
+            }
+            'A'..='F' => {
+                builder.push(
+                    c.to_lowercase()
+                        .next()
+                        .expect("Lexer: Impossible char conversion!"),
+                );
+                lexer.i(1);
+            }
+            '_' => lexer.i(1), // Skip
+            _ => break,
+        }
+    }
+
+    if builder.is_empty() {
+        panic!("Lexer: Hex number starting at {line}:{col} has no token at {}:{}. Format like 0x['0'..='9'|'a'..='f'|'A'..='F'|'_']", lexer.line, lexer.col);
+    }
+
+    tokens.push(PosToken::new(Token::NumberHex(builder), line, col));
+}
+
+// TODO: No panic
+fn get_binary(lexer: &mut Lexer, tokens: &mut Tokens) {
+    let (line, col) = (lexer.line, lexer.col);
+
+    let mut builder = String::new();
+    lexer.i(2);
+    while lexer.within() {
+        let c = lexer.peek();
+        match c {
+            '0' | '1' => {
+                builder.push(c);
+                lexer.i(1);
+            }
+            '_' => lexer.i(1), // Skip
+            _ => break,
+        }
+    }
+
+    if builder.is_empty() {
+        panic!("Lexer: Binary number starting at {line}:{col} has no token at {}:{}. Format like 0b['0'|'1'|'_']", lexer.line, lexer.col);
+    }
+
+    tokens.push(PosToken::new(Token::NumberBinary(builder), line, col));
+}
+
+// TODO: No panic
+fn get_number(lexer: &mut Lexer, tokens: &mut Tokens) {
+    let (line, col) = (lexer.line, lexer.col);
+    let mut builder = String::new();
+
+    let c = lexer.peek();
+    if c == '-' {
+        builder.push(c);
+        lexer.i(1);
+    }
+
+    while lexer.within() {
+        let c = lexer.peek();
+        match c {
+            '0'..='9' | '.' => {
+                builder.push(c);
+                lexer.i(1);
+            }
+            '_' => lexer.i(1), // Skip
+            _ => break,
+        }
+    }
+
+    if builder.is_empty() {
+        // TODO: I do not think this can be triggered. Remove.
+        panic!("Lexer: Number starting at {line}:{col} has no token at {}:{}. Format like ['-'|'0'..='9']['0'..='9'|'_']", lexer.line, lexer.col);
+    }
+
+    tokens.push(PosToken::new(Token::Number(builder), line, col));
+}
+
+// TODO: No panic
+fn get_char(lexer: &mut Lexer, tokens: &mut Tokens) {
+    let (line, col) = (lexer.line, lexer.col);
+    let mut builder = String::new();
+
+    lexer.i(1);
+    while lexer.within() {
+        let c = lexer.peek();
+        match c {
+            '\'' => {
+                lexer.i(1);
+                break;
+            }
+            '\0' => {
+                panic!("Lexer: Hit EOF when getting char at {line}:{col}. Missing closing \"'\".")
+            }
+            _ => {
+                builder.push(c);
+                lexer.i(1);
+            }
+        }
+    }
+
+    let mut c = '\0';
+    if builder.is_empty() {
+        panic!("Lexer: Char at {line}:{col} is empty.");
+    }
+    else if builder.len() > 1 {
+        if builder.eq("\n") {
+            c = '\n';
+        }
+        else if builder.eq("\t") {
+            c = '\t';
+        }
+    }
+    else {
+        c = builder
+            .chars()
+            .next()
+            .expect("Lexer: Missing char is impossible.");
+    }
+
+    tokens.push(PosToken::new(Token::Charater(c), line, col))
+}
+
+// TODO: No panic
+fn get_string(lexer: &mut Lexer, tokens: &mut Tokens) {
+    let (line, col) = (lexer.line, lexer.col);
+
+    let mut builder = String::new();
+    let mut backslash = false;
+
+    lexer.i(1);
+    while lexer.within() {
+        let c = lexer.peek();
+        match (c, backslash) {
+            ('"', false) => { lexer.i(1); break; },
+            ('"', true) => { backslash = false; builder.push(c); lexer.i(1); },
+            ('n', true) => { backslash = false; builder.push('\n'); lexer.i(1); },
+            ('t', true) => { backslash = false; builder.push('\t'); lexer.i(1); },
+            ('\\', false) => { backslash = true; lexer.i(1); },
+            ('\\', true) => { backslash = false; builder.push(c); lexer.i(1); },
+            (_, true) => panic!("Lexer: Hit a random '\\' at {}:{} when getting string at {line}:{col}.", lexer.line, lexer.col),
+            ('\n', _) => panic!("Lexer: Hit newline at {}:{} when getting string at {line}:{col}. Missing closing '\"'? Or use a raw string to et multiline support. (r\"\")", lexer.line, lexer.col),
+            ('\0', _) => panic!("Lexer: Hit EOF when getting string at {line}:{col}. Missing closing '\"'."),
+            _ => { builder.push(c); lexer.i(1); },
+        }
+    }
+
+    tokens.push(PosToken::new(Token::String(builder), line, col));
+}
+
+// TODO: No panic
+fn get_back_string(lexer: &mut Lexer, tokens: &mut Tokens) {
+    let (line, col) = (lexer.line, lexer.col);
+
+    let mut builder = String::new();
+    let mut backslash = false;
+
+    lexer.i(1);
+    while lexer.within() {
+        let c = lexer.peek();
+        match (c, backslash) {
+            (t, true) if !t.is_whitespace() => backslash = false,
+            ('`', _) => { lexer.i(1); break; },
+            ('\0', _) => panic!("Lexer: Hit EOF when getting back string at {line}:{col}. Missing closing '`'."),
+            _ => { builder.push(c); lexer.i(1); },
+        }
+    }
+
+    tokens.push(PosToken::new(Token::String(builder), line, col));
+}
+
 // TODO: More Testing
 #[cfg(test)]
 mod test {
+    use crate::lexer::Token;
+
     use super::lex;
 
     #[test]
+    fn only_single_line_comment() {
+        let tokens = lex(r##"// Single line comment life
+                                                      // // // // Single
+                                                      // Comment!!!       "##)
+        .unwrap();
+        assert_eq!(tokens.len(), 1);
+        assert_eq!(tokens[0].token, Token::EOF);
+    }
+
+    #[test]
     #[should_panic]
-    fn panic_multi_line() {
+    fn panic_multi_line_comment() {
         let _ = lex(r##"/* This comment does not have an ending!!!"##);
+    }
+
+    #[test]
+    fn get_hex() {
+        let tokens = lex(r##"0x1aAfFd90398536_24438f___12DDFffff"##).unwrap();
+        assert_eq!(tokens.len(), 2);
+        assert_eq!(
+            tokens[0].token,
+            Token::NumberHex("1aaffd9039853624438f12ddfffff".to_string())
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_get_hex_empty() {
+        let _ = lex(r##"0x"##);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_get_hex_bad_char() {
+        let _ = lex(r##"0xgg"##);
+    }
+
+    #[test]
+    fn get_binary() {
+        let tokens = lex(r##"0b1010__10000_01_01_011111___11101"##).unwrap();
+        assert_eq!(tokens.len(), 2);
+        assert_eq!(
+            tokens[0].token,
+            Token::NumberBinary("101010000010101111111101".to_string())
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_get_binary_empty() {
+        let _ = lex(r##"0b"##);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_get_binary_bad_char() {
+        let _ = lex(r##"0baa"##);
+    }
+
+    #[test]
+    fn get_numbers() {
+        let tokens = lex(r##"100 -110 3 1 -1 -0 -12 234__123 1_322 -1_234567689"##).unwrap();
+        assert_eq!(tokens.len(), 11);
+        assert_eq!(tokens[0].token, Token::Number("100".to_string()));
+        assert_eq!(tokens[1].token, Token::Number("-110".to_string()));
+        assert_eq!(tokens[2].token, Token::Number("3".to_string()));
+        assert_eq!(tokens[3].token, Token::Number("1".to_string()));
+        assert_eq!(tokens[4].token, Token::Number("-1".to_string()));
+        assert_eq!(tokens[5].token, Token::Number("-0".to_string()));
+        assert_eq!(tokens[6].token, Token::Number("-12".to_string()));
+        assert_eq!(tokens[7].token, Token::Number("234123".to_string()));
+        assert_eq!(tokens[8].token, Token::Number("1322".to_string()));
+        assert_eq!(tokens[9].token, Token::Number("-1234567689".to_string()));
+    }
+
+    #[test]
+    fn blank() {
+        let _ = lex(r##""##);
     }
 }
