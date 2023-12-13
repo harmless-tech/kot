aed37b6de5986bba36d8937dd6d76d877c0eb532
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-12-13 09:42:49 -0500

    Switched 64-bit types feature to 32-bit types feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,33 @@
 #![deny(unsafe_code)]
 #![allow(dead_code)] // TODO: Remove!!!
 
+// TODO: Allow lexer, parser, interpreter to be added as a feature independently.
 pub mod entry_args;
 pub mod lexer;
 pub mod platform;
 
 // TODO: Struct that is generated at compile time that holds feature info.
+// TODO: Kot uses COW semantics. (COW)
 
 // TODO: Move?
 /// 64 bit types
-#[cfg(feature = "64-bit-types")]
+#[cfg(not(feature = "32-bit-types"))]
 pub type TypeInt = i64;
 // 64 bit types
-//#[cfg(feature = "64-bit-types")]
+//#[cfg(not(feature = "32-bit-types"))]
 //pub type TypeUInt = u64;
 /// 64 bit types
-#[cfg(feature = "64-bit-types")]
+#[cfg(not(feature = "32-bit-types"))]
 pub type TypeFloat = f64;
 
 /// 32 bit types
-#[cfg(not(feature = "64-bit-types"))]
+#[cfg(feature = "32-bit-types")]
 pub type TypeInt = i32;
 // 32 bit types
-//#[cfg(not(feature = "64-bit-types"))]
+//#[cfg(feature = "32-bit-types")]
 //pub type TypeUInt = u32;
 /// 32 bit types
-#[cfg(not(feature = "64-bit-types"))]
+#[cfg(feature = "32-bit-types")]
 pub type TypeFloat = f32;
 
 // TODO: Move somewhere better?
